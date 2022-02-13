a37bede502ea375a7abf3e062660e9a4c48a049d
Shreyaan Seth <Shreyaans41@gmail.com>
2022-02-13 21:58:56 +0530

    sda

--- bot.js
+++ bot.js
@@ -10,12 +10,12 @@ require('dotenv').config();
 const token = process.env.TELEGRAM_TOKEN;
 let bot;
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
    bot = new TelegramBot(token);
    bot.setWebHook(process.env.HEROKU_URL + bot.token);
-} else {
-   bot = new TelegramBot(token, { polling: true });
-}
+// } else {
+//    bot = new TelegramBot(token, { polling: true });
+// }
 
 bot.onText(/\/start/, (msg) => {
 
