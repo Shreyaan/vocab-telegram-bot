05ca780013604d7fae2f5e032eb5025474259b7e
Shreyaan Seth <Shreyaans41@gmail.com>
2022-02-14 18:09:35 +0530

    l

--- bot.js
+++ bot.js
@@ -1,48 +1,46 @@
 // bot.js
 
-const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
-const parser = require('./parser.js');
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+const parser = require("./parser.js");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const filePath =('./words.csv');
+const filePath = "./words.csv";
 
-const csv = require('csv-parser')
-const fs = require('fs')
+const csv = require("csv-parser");
+const fs = require("fs");
 const results = [];
 
-const express = require('express')
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
-
- 
 
 const token = process.env.TELEGRAM_TOKEN;
 let bot;
 
-if (process.env.NODE_ENV === 'production') {
-   bot = new TelegramBot(token);
-   bot.setWebHook(process.env.HEROKU_URL + bot.token);
+if (process.env.NODE_ENV === "production") {
+  bot = new TelegramBot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
 } else {
-   bot = new TelegramBot(token, { polling: true });
+  bot = new TelegramBot(token, { polling: true });
 }
 
 // Start command
 bot.onText(/\/start/, (msg) => {
-
-    bot.sendMessage(msg.chat.id, `/word - send /word plus the word whose definition you want
+  bot.sendMessage(
+    msg.chat.id,
+    `/word - send /word plus the word whose definition you want
     
-/random- send this to get definition of a random word`);
-    
-    });
-
+/random- send this to get definition of a random word`
+  );
+});
 
 // Matches "/word whatever"
 bot.onText(/\/word (.+)/, (msg, match) => {
@@ -51,21 +49,24 @@ bot.onText(/\/word (.+)/, (msg, match) => {
   axios
     .get(`${process.env.OXFORD_API_URL}/entries/en-gb/${word}`, {
       params: {
-        fields: 'definitions',
-        strictMatch: 'false'
+        fields: "definitions",
+        strictMatch: "false",
       },
       headers: {
         app_id: process.env.OXFORD_APP_ID,
-        app_key: process.env.OXFORD_APP_KEY
-      }
+        app_key: process.env.OXFORD_APP_KEY,
+      },
     })
-    .then(response => {
+    .then((response) => {
       const parsedHtml = parser(response.data);
-      bot.sendMessage(chatId, parsedHtml, { parse_mode: 'HTML' });
+      bot.sendMessage(chatId, parsedHtml, { parse_mode: "HTML" });
     })
-    .catch(error => {
-      const errorText = error.response.status === 404 ? `No definition found for the word: <b>${word}</b>` : `<b>An error occured, please try again later</b>`;
-      bot.sendMessage(chatId, errorText, { parse_mode:'HTML'})
+    .catch((error) => {
+      const errorText =
+        error.response.status === 404
+          ? `No definition found for the word: <b>${word}</b>`
+          : `<b>An error occured, please try again later</b>`;
+      bot.sendMessage(chatId, errorText, { parse_mode: "HTML" });
     });
 });
 
@@ -75,31 +76,28 @@ bot.onText(/\/random/, (msg) => {
   let randomWord;
   let randomWordDef;
 
-  fs.createReadStream('words.csv')
-  .pipe(csv({ separator: '\t'}))
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    randomWord= results[lineNo]['word']
-    randomWordDef= results[lineNo]['definition']
-    bot.sendMessage(msg.chat.id, `Random Word:
+  fs.createReadStream("words.csv")
+    .pipe(csv({ separator: "\t" }))
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      randomWord = results[lineNo]["word"];
+      randomWordDef = results[lineNo]["definition"];
+      bot.sendMessage(
+        msg.chat.id,
+        `Random Word:
 ${randomWord}
   
 Definition:
-${randomWordDef}`);
-    
-  });
-
+${randomWordDef}`
+      );
+    });
 });
-
-
-
-
 
 app.use(bodyParser.json());
 
 app.listen(process.env.PORT);
 
-app.post('/' + bot.token, (req, res) => {
+app.post("/" + bot.token, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
